7f01b6e75e5484ce33e0be80fe68556991a083e3
tolbist4@gmail.com <AnatoliyStavskiy>
2024-11-26 18:49:04 +0300

    add CheckLink class(method getActualLink), add first TestLink

---
+++ src/main/java/ru/skyeng/ui/utils/CheckLink.java
@@ -0,0 +1,29 @@
+package ru.skyeng.ui.utils;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+public class CheckLink {
+    private WebDriver driver;
+
+    public CheckLink(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void switchToWindow(WebDriver driver) {
+        String actualWindow = driver.getWindowHandle();
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String windowHandle : allWindows) {
+            if (!windowHandle.equals(actualWindow)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+    }
+
+    public String getActualLink() {
+        switchToWindow(driver);
+        return driver.getCurrentUrl();
+    }
+}
