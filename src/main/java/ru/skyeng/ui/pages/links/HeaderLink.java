968167a07a117259d500e78c2ead011ab0cdcfd4
tolbist4@gmail.com <AnatoliyStavskiy>
2024-11-29 17:49:26 +0300

    Add in commint class HeaderLink

---
+++ src/main/java/ru/skyeng/ui/pages/links/HeaderLink.java
@@ -0,0 +1,5 @@
+package ru.skyeng.ui.pages.links;
+
+public class HeaderLink {
+    public static final String FOR_TEACHERS_LINK = "https://job.skyeng.ru/english-teachers?utm_source=skyeng&utm_medium=newbutton&utm_campaign=headerprepfromskyeng";
+}
