2538f65a214f1cc1f1405483acd204c58197601f
tolbist4@gmail.com <AnatoliyStavskiy>
2024-12-06 22:28:21 +0300

    add AdultLinkXpath and add Adult links for checking actual

---
+++ src/main/java/ru/skyeng/ui/pages/links/adult/AdultLink.java
@@ -0,0 +1,10 @@
+package ru.skyeng.ui.pages.links.adult;
+
+public class AdultLink {
+    public static final String PREMIUM_LINK = "https://skyeng.ru/premium/?from=main_new_menu";
+    public static final String TALK_CLUB_LINK = "https://skyeng.ru/self-study/?from=main_new_menu";
+    public static final String SELF_STUDY_LINK = "https://skyeng.ru/self-study/?from=main_new_menu";
+    public static final String STANDART_LINK = "https://skyeng.ru/adults/?from=main_new_menu";
+    public static final String TALKS_LINK = "https://skyeng.ru/talks/?utm_source=main&utm_medium=courses&from=main_new_menu";
+    public static final String COURSES_LINK = "https://skyeng.ru/programs/";
+}
---
+++ src/main/java/ru/skyeng/ui/pages/links/adult/AdultLinkXpath.java
@@ -0,0 +1,10 @@
+package ru.skyeng.ui.pages.links.adult;
+
+public class AdultLinkXpath {
+    public static final String ADULTS_PREMIUM_LINK = "//div[@class='header-menu-content ng-tns-c1903745324-1']//div[@class='product-catalog-content'][contains(text(),'Учите английский по индивидуальной программе вмест')]";
+    public static final String ADULTS_TALK_CLUB_LINK = "//div[@class='header-menu-content ng-tns-c1903745324-1']//div[@class='product-catalog-content'][contains(text(),'Практикуйте язык самостоятельно по книгам, видео, ')]";
+    public static final String ADULTS_SELF_STUDY_LINK = "//div[@class='header-menu-content ng-tns-c1903745324-1']//div[@class='product-catalog-title'][normalize-space()='Self-Study']";
+    public static final String ADULTS_STANDART_LINK = "//div[@class='header-menu-content ng-tns-c1903745324-1']//div[@class='product-catalog-content'][contains(text(),'Занимайтесь 1:1 с преподавателем для карьеры, учеб')]";
+    public static final String ADULTS_TALKS_LINK = "//div[@class='header-menu-content ng-tns-c1903745324-1']//div[@class='product-catalog-title'][normalize-space()='Talks']";
+    public static final String ADULTS_COURSES_LINK = "//div[@class='header-menu-content ng-tns-c1903745324-1']//div[@class='product-catalog-title'][contains(text(),'Курсы')]";
+}
