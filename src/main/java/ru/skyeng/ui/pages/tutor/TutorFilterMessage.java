9f89f4d10a3c2e2d5a4732538241fea33ed5cba5
tolbist4@gmail.com <AnatoliyStavskiy>
2024-12-07 18:51:00 +0300

    add Tutor messages

---
+++ src/main/java/ru/skyeng/ui/pages/tutor/TutorFilterMessage.java
@@ -0,0 +1,10 @@
+package ru.skyeng.ui.pages.tutor;
+
+public class TutorFilterMessage {
+    public static final String BEGINNER_MESSAGE = "Beginner Английский с нуля";
+    public static final String ELEMENTARY_MESSAGE = "Elementary";
+    public static final String PRE_INTERMEDIATE_MESSAGE = "Pre-intermediate";
+    public static final String INTERMEDIATE_MESSAGE = "Intermediate";
+    public static final String UPPER_INTERMEDIATE_MESSAGE = "Upper-intermediate";
+    public static final String ADVANCED_MESSAGE = "Advanced";
+}
